68114275a045915eebc1572988fa56c0af27ca28
Denilson <dlemus@harding.edu>
2023-01-26 12:20:10 -0600

    🤡 Fetch fake data

---
+++ src/components/Stories/Stories.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
+import { User } from "../typescript/types";
+import Story from "../Story/Story";
+
+export default function Stories() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const data = [...Array(20)].map((_, i) => ({
+      userId: faker.datatype.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      avatar: faker.image.avatar(),
+      password: faker.internet.password(),
+      birthdate: faker.date.birthdate(),
+      registeredAt: faker.date.past(),
+    }));
+
+    setUsers(data);
+  }, []);
+
+  return (
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-scroll">
+      {users.map((user) => (
+        <Story key={user.userId} image={user.avatar} username={user.username} />
+      ))}
+    </div>
+  );
+}
