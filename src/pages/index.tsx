f0ae1cae1058f96d2e48244d99ca07fe2ee264ae
Denilson <dlemus@harding.edu>
2023-01-27 15:34:01 -0600

    ✨ Display feed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
 import Navbar from "@/components/Navbar/Navbar";
+import Feed from "@/components/Feed/Feed";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
 
       <Navbar />
+      <Feed />
     </>
   );
 }
