1b208349f97325c4767b68e3d194c5172da496c1
Denilson <dlemus@harding.edu>
2023-01-27 16:05:32 -0600

    🚧 Work on posts component

---
+++ src/components/Posts/Posts.tsx
@@ -0,0 +1,38 @@
+import Post from "../Post/Post";
+
+export default function Posts() {
+  const posts = [
+    {
+      id: "111",
+      username: "user",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Avatar_icon_green.svg/2048px-Avatar_icon_green.svg.png",
+      caption: "caption",
+    },
+    {
+      id: "222",
+      username: "user",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Avatar_icon_green.svg/2048px-Avatar_icon_green.svg.png",
+      caption: "caption",
+    },
+    {
+      id: "333",
+      username: "user",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Avatar_icon_green.svg/2048px-Avatar_icon_green.svg.png",
+      caption: "caption",
+    },
+  ];
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          username={post.username}
+          image={post.image}
+          caption={post.image}
+        />
+      ))}
+    </div>
+  );
+}
