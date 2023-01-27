9cae877bc1c51f7086501f84ea853686a34756b2
Denilson <dlemus@harding.edu>
2023-01-27 15:41:23 -0600

    🚧 Wrok on feed component

---
+++ src/components/Feed/Feed.tsx
@@ -0,0 +1,12 @@
+import Stories from "../Stories/Stories";
+
+export default function Feed() {
+  return (
+    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
+      <section className="col-span-2">
+        <Stories />
+      </section>
+      <section></section>
+    </main>
+  );
+}
