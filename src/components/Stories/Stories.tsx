442e94cf784b3b85eaa19017efd262d1575b3b9f
Denilson <dlemus@harding.edu>
2023-01-27 15:41:05 -0600

    💄 Style stories component

--- src/components/Stories/Stories.tsx
+++ src/components/Stories/Stories.tsx
@@ -21,7 +21,7 @@ export default function Stories() {
   }, []);
 
   return (
-    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-scroll">
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-scroll scrollbar-hide">
       {users.map((user) => (
         <Story key={user.userId} image={user.avatar} username={user.username} />
       ))}
