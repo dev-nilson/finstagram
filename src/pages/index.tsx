00b5c2270743c805df4dae39ce12ee8bce8a00b9
Denilson <dlemus@harding.edu>
2023-02-01 13:11:23 -0600

    ✨ Add login logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase";
 import Navbar from "@/components/Navbar/Navbar";
 import Feed from "@/components/Feed/Feed";
+import Login from "@/components/Login/Login";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [user, setUser] = useAuthState(auth);
+
   return (
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
@@ -13,8 +18,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <Navbar />
-      <Feed />
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
+          <Feed />
+        </>
+      )}
     </div>
   );
 }
