18a021de6d5b32265370a2b1d940c5a77b728290
Denilson <dlemus@harding.edu>
2023-02-01 12:40:26 -0600

    🐛 Fix undefined variables

--- firebase.ts
+++ firebase.ts
@@ -1,18 +1,20 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "dev-nilson-finstagram.firebaseapp.com",
   projectId: "dev-nilson-finstagram",
   storageBucket: "dev-nilson-finstagram.appspot.com",
-  messagingSenderId: process.env.FIREBASE_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth();
 const db = getFirestore();
 const storage = getStorage();
 
-export { app, db, storage };
+export { app, auth, db, storage };
