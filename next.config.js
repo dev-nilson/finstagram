6f0d519a94108ab808a18721768a57d25226f138
Denilson <dlemus@harding.edu>
2023-02-01 16:40:12 -0600

    🔧 Add host name for images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["instagram.fsal2-1.fna.fbcdn.net", "lh3.googleusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
