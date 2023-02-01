29c72b2551ccc9b60bbc5a85b5901ec776f4a565
Denilson <dlemus@harding.edu>
2023-02-01 16:29:06 -0600

    ✨ Add dynamic profile info

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,16 +1,28 @@
+import { auth } from "../../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+
 export default function Profile() {
+  const [user, setUser] = useAuthState(auth);
+
+  console.log(user, "***");
+
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <img
         className="h-16 w-16 rounded-full border p-[2px]"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Avatar_icon_green.svg/2048px-Avatar_icon_green.svg.png"
+        src={user?.photoURL}
         alt="profile"
       />
       <div className="flex-1 mx-4">
-        <h2 className="font-semibold">username</h2>
-        <h3 className="text-sm text-gray-400">Name Lastname</h3>
+        <h2 className="font-semibold">{user?.email}</h2>
+        <h3 className="text-sm text-gray-400">{user?.displayName}</h3>
       </div>
-      <button className="text-blue-500 text-sm font-semibold">Sign Out</button>
+      <button
+        className="text-blue-500 text-sm font-semibold"
+        onClick={() => auth.signOut()}
+      >
+        Sign Out
+      </button>
     </div>
   );
 }
