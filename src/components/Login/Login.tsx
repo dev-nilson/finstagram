b831dfb2d181a880350db321c82b5c7d83e72fa0
Denilson <dlemus@harding.edu>
2023-02-01 13:40:44 -0600

    💄 Customize google button

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../../../firebase";
+import logo from "@/assets/google.jpg";
 
 function Login() {
   const googleAuth = new GoogleAuthProvider();
@@ -9,8 +11,18 @@ function Login() {
   };
 
   return (
-    <div>
-      <button onClick={login}>Login</button>
+    <div className="flex justify-center items-center h-[100vh]">
+      <button
+        className="flex items-center p-2 bg-blue-500 rounded-md text-white font-semibold hover:bg-blue-600 transition duration-200 ease-out"
+        onClick={login}
+      >
+        <Image
+          className="w-8 h-8 mr-3 rounded-sm"
+          src={logo}
+          alt="google icon"
+        />
+        Sign in with Google
+      </button>
     </div>
   );
 }
