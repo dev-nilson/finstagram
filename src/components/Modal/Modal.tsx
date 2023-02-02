d1dd4bb066627149ec62d91a574fea5851d449f7
Denilson <dlemus@harding.edu>
2023-02-02 13:02:48 -0600

    ✨ Add modal inputs

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -43,13 +43,38 @@ function Modal() {
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
             <div className="inline-block align-bottom bg-white rounded-md px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:pb-6">
-              <div className="mt-5 sm:mt-6">
-                <button
-                  type="button"
-                  className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-600 focus:outline-none sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
-                >
-                  Upload Post
-                </button>
+              <div>
+                <div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg leading-6 font-medium text-gray-900"
+                    >
+                      Upload Pictures
+                    </Dialog.Title>
+
+                    <div>
+                      <input type="file" hidden />
+                    </div>
+
+                    <div className="mt-2">
+                      <input
+                        className="border-none focus:ring-0 w-full text-center"
+                        type="text"
+                        placeholder="Enter caption..."
+                      />
+                    </div>
+                  </div>
+
+                  <div className="mt-5 sm:mt-6">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-600 focus:outline-none sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+                    >
+                      Upload Post
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </Transition.Child>
