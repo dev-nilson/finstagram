70176d4c79b1c3a3bd5bd65492d167c1c64e2e02
Denilson <dlemus@harding.edu>
2023-02-06 20:50:06 -0600

    🚧 Read posts from database

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,26 +1,19 @@
+import { useEffect, useState } from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../../../firebase";
 import Post from "../Post/Post";
 
 export default function Posts() {
-  const posts = [
-    {
-      id: "111",
-      username: "user",
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Avatar_icon_green.svg/2048px-Avatar_icon_green.svg.png",
-      caption: "caption",
-    },
-    {
-      id: "222",
-      username: "user",
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Avatar_icon_green.svg/2048px-Avatar_icon_green.svg.png",
-      caption: "caption",
-    },
-    {
-      id: "333",
-      username: "user",
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Avatar_icon_green.svg/2048px-Avatar_icon_green.svg.png",
-      caption: "caption",
-    },
-  ];
+  const [posts, setPosts] = useState<any>([]);
+
+  useEffect(() => {
+    return onSnapshot(
+      query(collection(db, "posts"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+  }, [db]);
 
   return (
     <div>
@@ -28,9 +21,10 @@ export default function Posts() {
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          image={post.image}
-          caption={post.caption}
+          username={post.data().username}
+          image={post.data().avatar}
+          caption={post.data().caption}
+          expected={post.data().expectedImage}
         />
       ))}
     </div>
