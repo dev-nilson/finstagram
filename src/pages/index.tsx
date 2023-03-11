d86fbd18b51e724bd338fef3b528ee822c2b3d58
Denilson <dlemus@harding.edu>
2023-03-11 00:06:14 -0600

    ✨ Add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,7 @@ export default function Home() {
       <Head>
         <title>Finstagram</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       {!user ? (
