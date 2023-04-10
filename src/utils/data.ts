a249ff9115190c78c1319f96248e3f883db4b93d
Denilson <dlemus@harding.edu>
2023-04-10 17:16:24 -0600

    🚚 Move suggestions data to separate folder

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -24,3 +24,14 @@ export const users = [
     username: "lemus",
   },
 ];
+
+export const suggestions = [
+  {
+    id: "000",
+    username: "denilemus",
+    name: "Denilson",
+    image:
+      "https://instagram.fsal2-1.fna.fbcdn.net/v/t51.2885-19/318298176_147011638114453_2928497119277690072_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fsal2-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=gO-mmpTpcxQAX8aBeYl&tn=8eWFVgs95tecC5hJ&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAtsJ_nDwWi-Ph51G70li3Wk1bhJ6p1KP3XPKeUCqTYIQ&oe=63E0859B&_nc_sid=8fd12b",
+    link: "https://www.instagram.com/denilemus",
+  },
+];
