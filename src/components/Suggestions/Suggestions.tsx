2dc7a29d3540fecb4c53bef9d96550bbb07fdb1b
Denilson <dlemus@harding.edu>
2023-04-10 17:18:49 -0600

    🐛 Fix not found profile issue

--- src/components/Suggestions/Suggestions.tsx
+++ src/components/Suggestions/Suggestions.tsx
@@ -1,17 +1,7 @@
 import Image from "next/image";
+import { suggestions } from "@/utils/data";
 
 export default function Suggestions() {
-  const suggestions = [
-    {
-      id: "000",
-      username: "denilemus",
-      name: "Denilson",
-      image:
-        "https://instagram.fsal2-1.fna.fbcdn.net/v/t51.2885-19/318298176_147011638114453_2928497119277690072_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fsal2-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=gO-mmpTpcxQAX8aBeYl&tn=8eWFVgs95tecC5hJ&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAtsJ_nDwWi-Ph51G70li3Wk1bhJ6p1KP3XPKeUCqTYIQ&oe=63E0859B&_nc_sid=8fd12b",
-      link: "https://www.instagram.com/denilemus",
-    },
-  ];
-
   return (
     <div className="mt-4 ml-10">
       <div className="flex justify-between text-sm mb-5">
@@ -22,10 +12,10 @@ export default function Suggestions() {
 
       {suggestions.map((suggestion) => (
         <div
-        key={suggestion.id}
-        className="flex items-center justify-between mt-3"
+          key={suggestion.id}
+          className="flex items-center justify-between mt-3"
         >
-          <img
+          <Image
             className="w-10 h-10 rounded-full border p-[2px]"
             width={100}
             height={100}
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,3 +1,5 @@
+import denilson from "../assets/profile.jpg"
+
 export const users = [
   {
     id: 0,
@@ -30,8 +32,7 @@ export const suggestions = [
     id: "000",
     username: "denilemus",
     name: "Denilson",
-    image:
-      "https://instagram.fsal2-1.fna.fbcdn.net/v/t51.2885-19/318298176_147011638114453_2928497119277690072_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fsal2-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=gO-mmpTpcxQAX8aBeYl&tn=8eWFVgs95tecC5hJ&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAtsJ_nDwWi-Ph51G70li3Wk1bhJ6p1KP3XPKeUCqTYIQ&oe=63E0859B&_nc_sid=8fd12b",
+    image: denilson,
     link: "https://www.instagram.com/denilemus",
   },
 ];
